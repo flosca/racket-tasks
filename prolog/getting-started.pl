56b63869c604201387c1013eb1d91f1e28272c9d
flosca <flosca@thinkpad.(none)>
2014-09-24 01:34:30 +0600

    upd getting-started and add new module

--- prolog/getting-started.pl
+++ prolog/getting-started.pl
@@ -1,47 +1,50 @@
-%  #1
+p1(X,Y,Z) :- Z=X+Y.
+p2(X,Y,Z) :- Z is X+Y.
 
-factorial(0, 1).
-factorial(1, 1).
-factorial(N, Res) :-
-	N > 0,
-	N1 is N - 1,
-	factorial(N1, Res1),
-	Res is N * Res1.
+%  #1
+factor(0, 1).
+factor(N, Res) :-
+  N > 0,
+  N1 is N - 1,
+  factor(N1, Res1),
+  Res is N * Res1.
+
 
 %  #2
-even(0).
 even(N) :- N mod 2 =:= 0.
 
-odd(0).
-odd(N) :- N mod 2 =\= 0.
+odd(N)  :- N mod 2 =\= 0.
+
+factor2(0, 1).
+factor2(1, 1).
+factor2(N, Res) :-
+    N > 1,
+    N2 is N - 2,
+    factor2(N2, Res1),
+    Res is N * Res1.
+
+
 
 %  #3
-oddDouble(1, 1).
-oddDouble(N, Res) :-
-  N > 0,
-	N1 is N - 2,
-	oddDouble(N1, Res1),
-	Res is N * Res1.
+sir(N, Res) :- 
+    even(N),
+    Res is N // 2.
+sir(N, Res) :- 
+    odd(N),
+    Res is (3 * N + 1).
 
-evenDouble(0, 1).
-evenDouble(N, Res) :-
-  N > 0,
-	N1 is N - 2,
-	evenDouble(N1, Res1),
-	Res is N * Res1.
-
-
-
-doubleFactorial(N, Res) :-
-      odd(N),
-      oddDouble(N, Res).
-
-doubleFactorial(N, Res) :-
-      even(N),
-      evenDouble(N, Res).
+sirakuz(0, _).
+sirakuz(N, A0) :-
+    sir(A0, Res),
+    write(Res),
+    nl,
+    N1 is N - 1,
+    sirakuz(N1, Res).
 
 
-%  #4
+
+% #4
+
 isDivisor(N, Res) :- N mod Res =:= 0.
 isDivisor(N, Res) :- Res * Res < N,
                      Acc is Res + 2,
@@ -50,25 +53,14 @@ isDivisor(N, Res) :- Res * Res < N,
 
 isPrime(2).
 isPrime(3).
-isPrime(N):- Res is 3,
+isPrime(N):- 
              N > 3 ,
              odd(N),
-             \+isDivisor(N, Res).
+             \+isDivisor(N, 3).
 
 
-%  #5
-nextElemInSeq(N, Res) :- 
-    even(N),
-    Res is round(N / 2).
-nextElemInSeq(N, Res) :- 
-    odd(N),
-    Res is (3 * N + 1).
 
-sirakuz(0, _) :- !.
-sirakuz(N, A0) :-
-        nextElemInSeq(A0, Res),
-        write(Res),
-        nl,
-        N1 is N - 1,
-        sirakuz(N1, Res).
+
+
+
 
